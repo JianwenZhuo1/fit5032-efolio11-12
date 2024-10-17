1b0eb372cba0dd41611a3deefb5a881c453af426
Jianwen Zhuo1 <jzhu0115@student.monash.edu>
2024-10-17 20:24:56 +1100

    update vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue(),
     vueDevTools(),
